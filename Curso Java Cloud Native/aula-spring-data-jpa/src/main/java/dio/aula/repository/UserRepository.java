4006251ab460a418ed4cb5ee18d5ac32ffe660d7
joaomoraesdev <jmoraes2003@hotmail.com>
2025-05-16 20:10:13 -0300

    fix: removendo query duplicada

--- Curso Java Cloud Native/aula-spring-data-jpa/src/main/java/dio/aula/repository/UserRepository.java
+++ Curso Java Cloud Native/aula-spring-data-jpa/src/main/java/dio/aula/repository/UserRepository.java
@@ -12,8 +12,4 @@ public interface UserRepository extends JpaRepository<Usuario, Integer> {
 
     @Query("SELECT u FROM Usuario u WHERE u.name LIKE %:nome%")
     List<Usuario> filtrarPorNome(@Param("nome") String nome);
-
-    @Query("SELECT u FROM Usuario u WHERE u.name LIKE %:nome%")
-    List<Usuario> listarTodos(@Param("nome") String nome);
-
 }
